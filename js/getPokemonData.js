9881c1f3463def6e2271c32936bcce2337ade13a
DanielGengel <daniel.gengel@outlook.com>
2026-05-26 13:27:41 +0200

    CSS Overlay

--- js/getPokemonData.js
+++ js/getPokemonData.js
@@ -191,6 +191,84 @@ function closeOverlay() {
 
 
 
+// function createOverlayHTML(pokemon) {
+
+//     let typesHTML = "";
+
+//     for (let i = 0; i < pokemon.types.length; i++) {
+//         const type = pokemon.types[i].type.name;
+
+//         typesHTML += `
+//             <div class="type ${type}">
+//                 ${type}
+//             </div>
+//         `;
+//     }
+
+//     let abilitiesHTML = "";
+
+// for (let i = 0; i < pokemon.abilities.length; i++) {
+//     abilitiesHTML += `
+//         <div>
+//             ${pokemon.abilities[i].ability.name}
+//         </div>
+//     `;
+// }
+
+
+// let statsHTML = "";
+
+// for (let i = 0; i < pokemon.stats.length; i++) {
+//     const stat = pokemon.stats[i];
+
+//     statsHTML += `
+//         <div>
+//             ${stat.stat.name}: ${stat.base_stat}
+//         </div>
+//     `;
+// }
+
+//     return `
+//     <div class="overlayBackground" id="overlayBackground">
+//         <div class="overlayCard ${pokemon.types[0].type.name}">
+
+//             <button onclick="closeOverlay()" class="closeBtn">X</button>
+
+//             <img
+//                 class="pokemonImage"
+//                 src="${pokemon.sprites.other["official-artwork"].front_default}"
+//             >
+
+//             <p class="pokemonNumber">#${pokemon.id}</p>
+
+//             <h2 class="pokemonName">${pokemon.name}</h2>
+
+//             <div class="pokemonInfo">
+//                 <div>Height: ${pokemon.height}</div>
+//                 <div>Weight: ${pokemon.weight}</div>
+//             </div>
+
+//             <h3>Abilities</h3>
+//             <div class="abilities">
+//                 ${abilitiesHTML}
+//             </div>
+
+//             <h3>Stats</h3>
+//             <div class="stats">
+//                 ${statsHTML}
+//             </div>
+
+//             <div class="types">
+//                 ${typesHTML}
+//             </div>
+
+//         </div>
+//     </div>
+// `;
+// }
+
+
+
 function createOverlayHTML(pokemon) {
 
     let typesHTML = "";
@@ -207,66 +285,144 @@ function createOverlayHTML(pokemon) {
 
     let abilitiesHTML = "";
 
-for (let i = 0; i < pokemon.abilities.length; i++) {
-    abilitiesHTML += `
-        <div>
-            ${pokemon.abilities[i].ability.name}
+    for (let i = 0; i < pokemon.abilities.length; i++) {
+        abilitiesHTML += `
+           
+                ${pokemon.abilities[i].ability.name}
+                ${i < pokemon.abilities.length - 1 ? "," : ""}
+          
+        `;
+    }
+
+    let statsHTML = "";
+
+    for (let i = 0; i < pokemon.stats.length; i++) {
+
+        const stat = pokemon.stats[i];
+
+         const statPercent = (stat.base_stat / 255) * 100;
+
+
+        statsHTML += `
+    <div class="statRow">
+
+        <div class="statName">
+            ${stat.stat.name}
         </div>
-    `;
-}
 
+        <div class="statBarContainer">
 
-let statsHTML = "";
-
-for (let i = 0; i < pokemon.stats.length; i++) {
-    const stat = pokemon.stats[i];
-
-    statsHTML += `
-        <div>
-            ${stat.stat.name}: ${stat.base_stat}
-        </div>
-    `;
-}
-
-    return `
-    <div class="overlayBackground" id="overlayBackground">
-        <div class="overlayCard ${pokemon.types[0].type.name}">
-
-            <button onclick="closeOverlay()" class="closeBtn">X</button>
-
-            <img
-                class="pokemonImage"
-                src="${pokemon.sprites.other["official-artwork"].front_default}"
+            <div 
+                class="statBar ${pokemon.types[0].type.name}"
+                style="width: ${statPercent}%"
             >
-
-            <p class="pokemonNumber">#${pokemon.id}</p>
-
-            <h2 class="pokemonName">${pokemon.name}</h2>
-
-            <div class="pokemonInfo">
-                <div>Height: ${pokemon.height}</div>
-                <div>Weight: ${pokemon.weight}</div>
-            </div>
-
-            <h3>Abilities</h3>
-            <div class="abilities">
-                ${abilitiesHTML}
-            </div>
-
-            <h3>Stats</h3>
-            <div class="stats">
-                ${statsHTML}
-            </div>
-
-            <div class="types">
-                ${typesHTML}
+                ${stat.base_stat}
             </div>
 
         </div>
+
     </div>
 `;
+    }
+
+    return `
+    <div class="overlayBackground ${pokemon.types[0].type.name}">
+
+    <div class="overlayCard ${pokemon.types[0].type.name}">
+
+        <button onclick="closeOverlay()" class="closeBtn">
+            X
+        </button>
+
+        <img
+            class="pokemonImage"
+            src="${pokemon.sprites.other["official-artwork"].front_default}"
+        >
+
+        <p class="pokemonNumber">
+            #${pokemon.id}
+        </p>
+
+        <h2 class="pokemonName">
+            ${pokemon.name}
+        </h2>
+
+        <div class="types">
+            ${typesHTML}
+        </div>
+
+        <div class="overlayTabs">
+
+            <button 
+                class="tabBtn ${pokemon.types[0].type.name} active"
+                onclick="showTab('more')"
+            >
+                MORE
+            </button>
+
+            <button 
+                class="tabBtn ${pokemon.types[0].type.name}"
+                onclick="showTab('stats')"
+            >
+                STATS
+            </button>
+
+        </div>
+
+            <div id="moreTab" class="tabContent active">
+
+                <div class="pokemonInfo">
+                    <div>Height: ${pokemon.height}</div>
+                    <div>Weight: ${pokemon.weight}</div>
+                </div>
+
+                <div class="abilities"><h3>Abilities: ${abilitiesHTML}</h3></div>
+
+               
+
+            </div>
+
+            <div id="statsTab" class="tabContent">
+
+                <div class="stats">
+                    ${statsHTML}
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    `;
 }
 
 
+function showTab(tab) {
 
+    const moreTab = document.getElementById("moreTab");
+    const statsTab = document.getElementById("statsTab");
 
+    const buttons = document.querySelectorAll(".tabBtn");
+
+    buttons.forEach(btn => {
+        btn.classList.remove("active");
+    });
+
+    if (tab === "more") {
+
+        moreTab.classList.add("active");
+        statsTab.classList.remove("active");
+
+        buttons[0].classList.add("active");
+
+    } else {
+
+        statsTab.classList.add("active");
+        moreTab.classList.remove("active");
+
+        buttons[1].classList.add("active");
+    }
+}
+
+window.showTab = showTab;
+window.closeOverlay = closeOverlay;
